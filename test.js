d4dada1934f5be4332c7fb2179f7bb4ef9a32ee4
ken <block24block@gmail.com>
2018-09-14 00:01:13 +0800

    test ateles

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
+/**
+ * Created by turnon on 2018/9/14.
+ */
+// ==UserScript==
+// @name         test_ateles
+// @namespace    https://github.com/turnon/test_ateles
+// @version      0.0.1
+// @description  test_ateles
+// @author       block24block@gmail.com
+// @match       *://movie.douban.com/*/photos*
+// @grant        none
+// @require https://greasyfork.org/scripts/372188-ateles/code/ateles.js?version=628769
+// ==/UserScript==
 Ateles(['test/t1', 'test/t3'], function (t1, t3) {
     console.log(t1.minus3(5) * t3.plus3(2))
 })
